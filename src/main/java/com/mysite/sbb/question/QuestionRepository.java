df06aaa226ec12e700a85a27092e8a2a3a1578cd
yongbbbba <lisagamer123@gmail.com>
2022-12-10 23:15:32 +0900

    make paging

--- src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -2,10 +2,13 @@ package com.mysite.sbb.question;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer>{ // Repository 대상이 되는 엔터티와 그 엔터티의 기본키를 입력
 	Question findBySubject(String subject); // 이 메소드가 실행될 때 JPA가 해당 메서드명을 분석하여 쿼리를 만들고 실행한다. findBy + 엔터티의 속성명
 	Question findBySubjectAndContent(String subject, String content);
 	List<Question> findBySubjectLike(String subject);
+	Page<Question> findAll(Pageable pageable);
 }
--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -10,20 +10,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionRepository;
+import com.mysite.sbb.question.QuestionService;
 
 @SpringBootTest
 class SbbApplicationTests {
 	
 	@Autowired
-	private QuestionRepository questionRepository;
+	private QuestionService questionService;
 
 	@Test
 	void testJpa() {
-		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
-		
-		Question q = qList.get(0);
-		 assertEquals("sbb가 무엇인가요?", q.getSubject());
-		
+		for (int i=1; i<=300; i++) {
+			String subject = String.format("테스트 데이터입니다:[%03d]", i);
+			String content = "내용무";
+			this.questionService.create(subject, content);
+					
+		}
 	}
-
 }
